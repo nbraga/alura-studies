ba63c4a303535639659d67cc9c9ce1f7ec9ea7d7
nicolas <nicolas.braga@tarkuss.com.br>
2022-08-16 13:55:58 -0400

    fix-add criando novas páginas e utilizando o Sass para estilização

---
+++ src/pages/App.tsx
@@ -0,0 +1,15 @@
+
+import Form from '../components/Form'
+import Lista from '../components/Lista'
+import './style.scss'
+
+function App() {
+  return (
+    <div className='AppStyle'>
+   <Form/>
+   <Lista/>
+    </div>
+  );
+}
+
+export default App;
