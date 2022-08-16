d93a4b0d26a515403b6df1abdd239c2cb7ed959e
nicolas <nicolas.braga@tarkuss.com.br>
2022-08-16 16:32:26 -0400

    fix-update Atualizando as pastas e iniciando os estudos com Typescript

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,29 @@
-
-import Form from '../components/Form'
-import Lista from '../components/Lista'
-import './style.scss'
+import { useState } from "react";
+import Cronometro from "../components/Cronometro";
+import Form from "../components/Form";
+import Lista from "../components/Lista";
+import style from "./App.module.scss";
 
 function App() {
+  const [tarefas, setTarefas] = useState([
+    {
+      tarefa: "React",
+      tempo: "02:00:00",
+    },
+    {
+      tarefa: "Javascript",
+      tempo: "01:00:00",
+    },
+    {
+      tarefa: "Typescript",
+      tempo: "03:00:00",
+    },
+  ]);
   return (
-    <div className='AppStyle'>
-   <Form/>
-   <Lista/>
+    <div className={style.AppStyle}>
+      <Form setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} />
+      <Cronometro />
     </div>
   );
 }
